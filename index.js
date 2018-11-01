daecb334ac737e3f83c7a8f58c00fd9fba0ed1c8
Pakpoom Yimsukanan <momepukku@gmail.com>
2018-11-01 11:54:50 +0700

    add second test case

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const changes = [1, 2, 5, 10];
+const changes = [1, 2, 5, 10, 20, 100, 500];
 
 const exchange = (price, input) => {
-
+    return []
 };
 
 export default exchange
--- index.test.js
+++ index.test.js
@@ -3,6 +3,10 @@ import exchange from './index'
 describe('exchange', function () {
     it('return empty array if no change', () => {
         const result = exchange(10, 10);
-        expect(result).toBe([]);
-    })
+        expect(result).toEqual([]);
+    });
+    it('return [10] if change is 10', () => {
+        const result = exchange(10, 20);
+        expect(result).toEqual([10]);
+    });
 });
